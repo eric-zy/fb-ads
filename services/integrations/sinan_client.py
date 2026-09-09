7c96e91dba2fa70ec98417d479d3920f0acd530b
eric_zy <1935152040@qq.com>
2026-09-09 10:50:37 +0800

    按司南当前账号接口动态获取推广应用

--- services/integrations/sinan_client.py
+++ services/integrations/sinan_client.py
@@ -21,7 +21,7 @@ class SinanClient:
     async def promotion_list(self, page=1, page_size=20): return await self.request('POST', '/delivery/promotion/query/v1', json={'page': page, 'page_size': page_size})
     async def promotion_detail(self, promotion_id): return await self.request('GET', '/delivery/promotion/query_by_id/v1', params={'promotion_id': promotion_id})
     async def create_promotion(self, payload): return await self.request('POST', '/delivery/promotion/create/v1', json=payload)
-    async def app_tree(self): return await self.request('GET', '/account/org_tree/v1')
+    async def app_tree(self): return await self.request('GET', '/account/get_current/v1')
     async def filter_options(self): return await self.request('GET', '/delivery/filter_options/v1')
     async def pixels(self, media_channel, real_app_id): return await self.request('GET', '/delivery/promotion/pixel/query/v1', params={'media_channel': media_channel, 'real_app_id': real_app_id})
     async def recharge_templates(self, real_app_id): return await self.request('GET', '/delivery/recharge_template/query/v1', params={'page': 1, 'page_size': 100, 'dis_app_id': real_app_id})
